2acfe65ced2a1d3d3bb939e2199df5ff4640f11a
olga.krekhovetska <olga.krekhovetska@eleks.com>
2014-11-26 18:25:37 +0200

    initial commit.

---
+++ com.test.jenkins/src/main/java/com/test/jenkins/App.java
@@ -0,0 +1,14 @@
+package com.test.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		System.out.println( ei ghttry!");
+  }
+}
