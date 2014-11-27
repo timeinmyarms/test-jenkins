3e8b3f2043c5cd399c2cd4bc223d8d43a65e26e9
olga.krekhovetska <olga.krekhovetska@eleks.com>
2014-11-27 16:16:14 +0200

    commit.

--- com.test.jenkins/src/main/java/com/test/jenkins/App.java
+++ com.test.jenkins/src/main/java/com/test/jenkins/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		System.out.println( tenth ghttry!");
+		System.out.println( eleventh ghttry!");
   }
 }
