b1abf06fa25cf3ae521a95b4881c9713f3416ade
olga.krekhovetska <olga.krekhovetska@eleks.com>
2014-11-27 13:34:36 +0200

    tenth.

--- com.test.jenkins/src/main/java/com/test/jenkins/App.java
+++ com.test.jenkins/src/main/java/com/test/jenkins/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		System.out.println( ninth ghttry!");
+		System.out.println( tenth ghttry!");
   }
 }
